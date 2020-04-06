500e2c2d40c58d36cffbda81aa0a937f367adf41
Justin Heyes-Jones <justin.heyes-jones@yoppworks.com>
2020-04-06 10:35:03 -0700

    my shopping cart save

--- src/main/scala/org/justinhj/Myshoppingcartclient.scala
+++ src/main/scala/org/justinhj/Myshoppingcartclient.scala
@@ -4,6 +4,8 @@ import io.grpc.ManagedChannelBuilder
 import com.example.shoppingcart.shoppingcart.GetShoppingCart
 import com.example.shoppingcart.shoppingcart.ShoppingCartGrpc
 import com.example.shoppingcart.shoppingcart.Cart
+import com.example.shoppingcart.shoppingcart.AddLineItems
+import com.example.shoppingcart.shoppingcart.LineItem
 
 object Myshoppingcartclient {
 
@@ -12,15 +14,29 @@ object Myshoppingcartclient {
   val host = "localhost"
   val port = 9000
 
-  println("Making call")
-
   val channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext.build
-
-  val request = GetShoppingCart(userId = "Justin1")
-
   val blockingStub = ShoppingCartGrpc.blockingStub(channel)
-  val reply: Cart = blockingStub.getCart(request)
-  println(reply)
+
+  val userId = "Justin1"
+
+   // Put multiple items in the cart
+  val addResult = blockingStub.addItems(
+    AddLineItems(userId,
+      List(LineItem("BARN109-120", "Barney the Dinosaur, Large plushy", 1),
+            LineItem("DYSO101-121", "Dyson Vaccuum Super", 2),
+            LineItem("CIND129-532", "Cindy Doll - Go to Party", 1))))
+
+  println(addResult)
+
+  // Get a Cart
+  val cart: Cart = blockingStub.getCart(GetShoppingCart(userId))
+  println(cart)
+
+  // Can also do this async like this
+
+  // val stub = ShoppingCartGrpc.stub(channel)
+  // val f: Future[Cart] = stub.getCart(request)
+  // f onComplete println
 
  }
 }
